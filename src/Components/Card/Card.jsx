7e19e35ea83e706e796b7bd5e5ba9210038ad237
Philipp R. Proksch <mail@prp.wien>
2022-02-09 16:03:10 +0000

    🎨 changed imported menu image to inline svg

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Card.css";
 
-//Images
-import Ellipsis from "../../Images/icon-ellipsis.svg";
-
 //Components
 import CardIcon from "./CardIcon";
 import CardBody from "./CardBody";
@@ -40,7 +37,9 @@ function Card(props) {
       <div id="card">
         <div id="header">
           <h1>{props.title}</h1>
-          <img src={Ellipsis} alt="Menu" />
+          <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" />
+          </svg>
         </div>
         <CardBody timeframe={props.timeframe} timeframes={props.timeframes} />
       </div>

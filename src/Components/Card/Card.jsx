1a03071bc36720fa49c14ff516b603b6417488e5
Philipp R. Proksch <mail@prp.wien>
2022-02-09 09:26:18 +0000

    ✨ imported Icons

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,6 +1,13 @@
-import Ellipsis from "../../Images/icon-ellipsis.svg";
-
 import "./Card.css";
+
+//Images
+import Ellipsis from "../../Images/icon-ellipsis.svg";
+import IconWork from "../../Images/icon-work.svg";
+import IconExercise from "../../Images/icon-exercise.svg";
+import IconPlay from "../../Images/icon-play.svg";
+import IconSelfCare from "../../Images/icon-self-care.svg";
+import IconSocial from "../../Images/icon-social.svg";
+import IconStudy from "../../Images/icon-study.svg";
 
 function Card(props) {
   return (

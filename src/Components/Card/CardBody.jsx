2f45179f12a4dcf37609adf71f84cd5d2810028e
Philipp R. Proksch <mail@prp.wien>
2022-02-09 15:39:59 +0000

    🔥 removed imported images

--- src/Components/Card/CardBody.jsx
+++ src/Components/Card/CardBody.jsx
@@ -1,11 +1,3 @@
-//Images
-import IconWork from "../../Images/icon-work.svg";
-import IconExercise from "../../Images/icon-exercise.svg";
-import IconPlay from "../../Images/icon-play.svg";
-import IconSelfCare from "../../Images/icon-self-care.svg";
-import IconSocial from "../../Images/icon-social.svg";
-import IconStudy from "../../Images/icon-study.svg";
-
 function CardBody(props) {
   return (
     <>

e0105fc9ff3d1fc147c2a807c020c24c27d57a33
Philipp R. Proksch <mail@prp.wien>
2022-02-09 12:50:11 +0000

    🎨 added classNames and IDs

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -12,14 +12,14 @@ import IconStudy from "../../Images/icon-study.svg";
 function Card(props) {
   return (
     <>
-      <div>
-        <div>
-          <h2>{props.title}</h2>
+      <div className="Card">
+        <div id="header">
+          <h1>{props.title}</h1>
           <img src={Ellipsis} alt="Menu" />
         </div>
         <div>
-          <h1>{props.timeframes.daily.current}hrs</h1>
-          <h2>Previous - {props.timeframes.daily.previous}hrs</h2>
+          <p id="time">{props.timeframes.daily.current}hrs</p>
+          <p id="previous">Previous - {props.timeframes.daily.previous}hrs</p>
         </div>
       </div>
     </>

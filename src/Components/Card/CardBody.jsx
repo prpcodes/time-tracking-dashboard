58af83e88f458df2e7340fc5a1f0904db85db495
Philipp R. Proksch <mail@prp.wien>
2022-02-10 12:41:02 +0000

    ✨ changed text for previous to fit the timeframe

--- src/Components/Card/CardBody.jsx
+++ src/Components/Card/CardBody.jsx
@@ -4,19 +4,21 @@ function CardBody(props) {
       {props.timeframe === "Daily" ? (
         <div id="body">
           <p id="time">{props.timeframes.daily.current}hrs</p>
-          <p id="previous">Previous - {props.timeframes.daily.previous}hrs</p>
+          <p id="previous">Yesterday - {props.timeframes.daily.previous}hrs</p>
         </div>
       ) : null}
       {props.timeframe === "Weekly" ? (
         <div id="body">
           <p id="time">{props.timeframes.weekly.current}hrs</p>
-          <p id="previous">Previous - {props.timeframes.weekly.previous}hrs</p>
+          <p id="previous">Last Week - {props.timeframes.weekly.previous}hrs</p>
         </div>
       ) : null}
       {props.timeframe === "Monthly" ? (
         <div id="body">
           <p id="time">{props.timeframes.monthly.current}hrs</p>
-          <p id="previous">Previous - {props.timeframes.monthly.previous}hrs</p>
+          <p id="previous">
+            Last Month - {props.timeframes.monthly.previous}hrs
+          </p>
         </div>
       ) : null}
     </>

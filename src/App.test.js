7fc4585ee8e68d573f1b8586cd56c4d3f5e343ba
Philipp R. Proksch <mail@prp.wien>
2022-02-11 13:04:54 +0000

    ✅ added tests for timeframe changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders the timeframes", () => {
@@ -9,4 +9,25 @@ test("renders the timeframes", () => {
   expect(daily).toBeInTheDocument();
   expect(weekly).toBeInTheDocument();
   expect(monthly).toBeInTheDocument();
+});
+
+test("changes the timeframe to daily", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("Daily"));
+  const yesterday = screen.getAllByText(/Yesterday/i);
+  expect(yesterday.length).toEqual(6);
+});
+
+test("changes the timeframe to weekly", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("Weekly"));
+  const yesterday = screen.getAllByText(/Last Week/i);
+  expect(yesterday.length).toEqual(6);
+});
+
+test("changes the timeframe to monthly", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("Monthly"));
+  const lastMonth = screen.getAllByText(/Last Month /i);
+  expect(lastMonth.length).toEqual(6);
 });
